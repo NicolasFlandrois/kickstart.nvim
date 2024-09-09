e54059c80b06f82d174c6552eec0a9fb7cfd83fa
Nicolas Flandrois <nicolas@flandrois.com>
2024-09-09 15:38:32 -0400

    Initial Basic Custom Nvim Config
    VimWiki + LoreIpsum
    Kept in comments 2 other packages that I intend to test out: NeoGit and codium AI

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,61 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  --[[{
+    --
+    -- NeoGit
+    --
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+    },
+    config = true,
+  },]]
+  {
+    --
+    -- VimWiki
+    --
+    'vimwiki/vimwiki',
+    -- config = function()
+    init = function()
+      vim.g.vimwiki_list = {
+        { path = '~/.MyNotes/', syntax = 'markdown', ext = '.md', links_space_char = '-' },
+        -- { path = '~/st/pm/work_notes', syntax = 'markdown', ext = '.md', links_space_char = '-' },
+      }
+      vim.g.vimwiki_ext2syntax = { ['.md'] = 'markdown', ['.markdown'] = 'markdown', ['.mdown'] = 'markdown' }
+      vim.g.vimwiki_use_mouse = 1
+      vim.g.vimwiki_markdown_link_ext = 1
+    end,
+  },
+  {
+    -- Lorem Ipsum
+    'derektata/lorem.nvim',
+    config = function()
+        require('lorem').opts {
+            sentenceLength = "medium",
+            comma_chance = 0.2,
+            max_commas_per_sentence = 2,
+        }
+    end
+  },
+
+  --  {
+  --  --
+  --  -- Codeium (Copilot FOSS alternative)
+  --  --
+  --    'Exafunction/codeium.nvim',
+  --    dependencies = {
+  --      'nvim-lua/plenary.nvim',
+  --      'hrsh7th/nvim-cmp',
+  --    },
+  --    config = function()
+  --      require('codeium').setup {}
+  --    end,
+  --  },
+}
